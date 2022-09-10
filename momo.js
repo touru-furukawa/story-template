c2af4b1a30480b7e437f97edd615abdfa92fca8a
touru <torufurukawa+touru@gmail.com>
2022-09-11 00:23:08 +0900

    Clean

--- momo.js
+++ momo.js
@@ -6,12 +6,15 @@ const next = spawn("npx", ["next-remote-watch"])
 next.stdout.on("data", data => {
   process.stdout.write(`${data}`)
 })
+
 next.stderr.on("data", data => {
   process.stderr.write(`${data}`)
 })
+
 next.on("error", error => {
   process.stderr.write(`${error.message}`)
 })
+
 next.on("close", code => {
   process.stderr.write(`exit: ${code}`)
 })
